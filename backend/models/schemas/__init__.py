42c74b9c895349f44ff7daae4a7a29ef40b2007e
arshdeepkaur190106 <arshdeepkaur190106@gmail.com>
2026-07-08 14:15:00 +0530

    Remove empty duplicate schemas folder from models

--- backend/models/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-schemas package
-
-Contains Pydantic schemas that validate data flowing in and out of the
-FastAPI routes (request bodies and response models). These govern the
-API contract, separate from how data is actually stored in MongoDB.
-"""
